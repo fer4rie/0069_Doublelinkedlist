b6e9d8208f8d89967fc906a4848a76508cf38170
fer4rie <firapuspita2106@gmail.com>
2026-04-30 21:11:15 +0700

    step 1 : allocate memory for new node

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -27,3 +27,6 @@ public:
         int nim;
         cout << "\nEnter the roll number of the student: ";
         cin >> nim;
+
+        // step 1: Allocate memory for new node
+        Node *newNode = new Node();
