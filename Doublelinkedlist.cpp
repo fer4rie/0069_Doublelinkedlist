1e3f6669a10a1a4cab17e4ab51261dfa24bb5691
fer4rie <firapuspita2106@gmail.com>
2026-04-30 21:48:03 +0700

    step 1 : Move to last node

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -160,3 +160,12 @@ public:
             cout << "\nList is empty" << endl;
             return;
         }
+
+        // step 1: Move to last node
+        Node *currentNode = START;
+        int i = 0;
+        while (currentNode->next != NULL)
+        {
+            currentNode = currentNode->next;
+            i++;
+        }
